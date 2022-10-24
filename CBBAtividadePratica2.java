0b346d03fa4258c73d7f83a2056757fcf1ba66ba
Thierry Silva <thierryfulgencio@gmail.com>
2022-10-24 19:12:43 -0300

    Implementação das questões da primeira atividade

---
+++ CBBAtividadePratica2.java
@@ -0,0 +1,15 @@
+class CBBAtividadePratica2 {
+    public static void main(String[] args) {
+
+        int number = 0;
+
+        System.out.print("Numeros de 0 a 100: ");
+
+        do {
+            
+            System.out.print(number + ",");
+            number++;
+
+        } while(number<=100);
+    }
+}
---
+++ CBBAtividadePratica3.java
@@ -0,0 +1,17 @@
+class CBBAtividadePratica3 {
+    public static void main(String[] args) {
+
+        int number;
+
+        System.out.print("Numeros pares de 0 a 100: ");
+        
+        for(number = 0; number <= 100; number++){
+
+            if(number % 2 == 0){
+
+                System.out.print(number + ",");
+
+            }
+        }
+    }
+}
